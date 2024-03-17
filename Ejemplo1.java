c551f00f5926b07706700e6606427f2896e09d61
Tovill <Tomas_8978@hotmail.com>
2024-03-16 20:33:45 -0600

    Cambio

--- Ejemplo1.java
+++ Ejemplo1.java
@@ -1,32 +1,31 @@
 //Ejemplo1 Realiza una simple suma orientado a objetos
-import java.util.Scanner;
-public class Ejemplo1 
+//para empezar, en ya debemos de tener en cuenta lo fundamental de la programacion para  poder entender poo
+//En este ejercicio se tiene en cuenta cosas ya usadas pero con la diferencia de que ahora debemos de tener el concepto de que es un metodo y clase
+//Empezaremos con una simple suma 
+public class Ejemplo1 //nombre de la clase inicial
+
 {
-    //private Ejemplo1()
-    //{ 
-    //}
-
-    public void HacerSuma(int a, int b, int c)
+    public int Suma(int num1,int num2)//metodo de suma que solo suma 2 numeros enteros, y cuyos datos son enteros 
     {
-       
-        Scanner scan= new Scanner(System.in);
-        System.out.println("Digita el primer numero a sumar ");
-        a=scan.nextInt();
-
-        System.out.println("Digita el segundo numero a sumar ");
-        b=scan.nextInt();
-
-        c=a+b;
-
-        System.out.println("El resultado es :" + c);
-        scan.close();
+        return num1+num2;//devuelve los numeros enteros y realiza la suma
     }
 
-    public static void main(String args[])
+    public double Suma( double num1,double num2)//metodo de suma para numeros decimales, los valores deben de ser decimales
     {
-        Ejemplo1 Suma = new Ejemplo1();
-        int a=0,b=0,c=0;
+        return num1+num2;//devuelve los numeros decimales y realiza la suma
+    }
 
-        Suma.HacerSuma(a,b,c);
+    public static void main(String[] args) 
+    {
+        // Crear una instancia de la clase Sumador
+        Ejemplo1 sumador = new Ejemplo1();
+        
+        // Sumar dos números enteros
+        int resultadoEntero = sumador.Suma(5, 3);
+        System.out.println("La suma de 5 y 3 es: " + resultadoEntero);
+        
+        // Sumar dos números decimales
+        double resultadoDecimal = sumador.Suma(2.5, 3.7);
+        System.out.println("La suma de 2.5 y 3.7 es: " + resultadoDecimal);
     }
 }
